d46817d887565b8a083d51b5f2585d3d1cb7094e
Harshita Jain <harshitaforever06@gmail.com>
2023-01-20 07:34:19 +0530

    Add pages

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -24,39 +24,43 @@ const Calculator = () => {
   };
 
   return (
-    <div className="h-[100vh] flex justify-center items-center">
-      <ul className="bg-[#dbc4c4] w-4/5 grid columns-6 border-2 border-black/40">
-        <input className="p-2 text-right justify-self-end bg-[#aaacbd] w-full text-white hover:bg-[#858694] placeholder:text-white output" placeholder="0" readOnly />
-        <ul className="flex justify-center items-center">
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>AC</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>+/-</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>%</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>÷</button></li>
+    <div className="grid grid-cols-2 ">
+      <h2 className="flex justify-center text-4xl py-10">Lets Do Some Math...</h2>
+
+      <div className="h-[100vh] flex justify-center items-center">
+        <ul className="bg-[#dbc4c4] w-4/5 grid columns-6 border-2 border-black/40">
+          <input className="p-2 text-right justify-self-end bg-[#aaacbd] w-full text-white hover:bg-[#858694] placeholder:text-white output" placeholder="0" readOnly />
+          <ul className="flex justify-center items-center">
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>AC</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>+/-</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>%</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>÷</button></li>
+          </ul>
+          <ul className="flex justify-center items-center">
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>7</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>8</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>9</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>x</button></li>
+          </ul>
+          <ul className="flex justify-center items-center">
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>4</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>5</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>6</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>-</button></li>
+          </ul>
+          <ul className="flex justify-center items-center">
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>1</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>2</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>3</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>+</button></li>
+          </ul>
+          <ul className="flex justify-center items-center">
+            <li className="flex justify-center border-2 border-black w-1/2"><button className="w-full" type="button" onClick={changeHandler}>0</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>.</button></li>
+            <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>=</button></li>
+          </ul>
         </ul>
-        <ul className="flex justify-center items-center">
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>7</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>8</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>9</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>x</button></li>
-        </ul>
-        <ul className="flex justify-center items-center">
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>4</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>5</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>6</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>-</button></li>
-        </ul>
-        <ul className="flex justify-center items-center">
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>1</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>2</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>3</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>+</button></li>
-        </ul>
-        <ul className="flex justify-center items-center">
-          <li className="flex justify-center border-2 border-black w-1/2"><button className="w-full" type="button" onClick={changeHandler}>0</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>.</button></li>
-          <li className="flex justify-center border-2 border-black w-1/4"><button className="w-full" type="button" onClick={changeHandler}>=</button></li>
-        </ul>
-      </ul>
+      </div>
     </div>
   );
 };
